c4cf99f2134c238d7f5ae2b04c04b3bccb35abf7
Le Deng <lede@Les-MacBook-Pro.local>
2016-05-03 13:28:13 -0400

    fixed add image folder

--- backend/src/main/java/com/lagoon/backend/controller/PhotoResource.java
+++ backend/src/main/java/com/lagoon/backend/controller/PhotoResource.java
@@ -45,7 +45,7 @@ public class PhotoResource {
         String fileName = multipartFile.getOriginalFilename();
         imageName=fileName;
 
-        String path = new File("src/main/resources/static/images").getAbsolutePath()+"/"+fileName;
+        String path = new File("target/classes/static/images").getAbsolutePath()+"/"+fileName;
         try {
             multipartFile.transferTo(new File(path));
             System.out.println(path);
